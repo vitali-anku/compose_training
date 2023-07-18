d0b497e47de01a9c1d06b744b33be76379e56dc4
ankek <vitalja.ja@gmail.com>
2023-07-18 22:37:35 +0300

    Update gradle and Screen.

---
+++ app/src/main/java/com/ankek/compoese_training/HomeScreen.kt
@@ -0,0 +1,11 @@
+package com.ankek.compoese_training
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun HomeScreen() {
+    Text(
+        text = "Hello. This is home screen."
+    )
+}
